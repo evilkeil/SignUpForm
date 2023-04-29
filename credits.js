3416971c8dae849bac317e34ba8fa78f6bb60fb3
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-04-29 20:55:17 +0900

    make the credits appear alongside the background img

--- credits.js
+++ credits.js
@@ -1,14 +1,26 @@
-const credit = document.querySelector('.credit');
-const imageContainer = document.querySelector('.image-container');
+const images = [
+    {src: 'images/leo-maverick-Vb5xjkLaFXQ-unsplash.jpg', credit: 'Leo Maverick'},
+    {src: 'images/marek-piwnicki-9celAnrs-BU-unsplash.jpg', credit: 'Marek Piwnicki'},
+    {src: 'images/sahil-moosa-q5O9V6yZFuk-unsplash.jpg', credit: 'Sahil Moosa'},
+    {src: 'images/tharoushan-kandarajah-KtDXt7DyfVM-unsplash.jpg', credit: 'Tharoushan Kandarajah'}
+];
 
-function setCredits(){
-    if(imageContainer.style.cssText="background-image: url(images/leo-maverick-Vb5xjkLaFXQ-unsplash.jpg);"){
-        credit.innerHTML = 'Photo by<a href="https://unsplash.com/@leomaverick?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText%22%3ELeo Maverick</a> on <a href="https://unsplash.com/photos/Vb5xjkLaFXQ?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText%22%3EUnsplash</a>';
-    }else if(imageContainer.style.cssText=" background-image: url(images/marek-piwnicki-9celAnrs-BU-unsplash.jpg);"){
-        credit.innerHTML ='Photo by <a href="https://unsplash.com/@marekpiwnicki?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText%22%3EMarek Piwnicki</a> on <a href="https://unsplash.com/photos/9celAnrs-BU?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText%22%3EUnsplash</a>'
-    }else if(imageContainer.style.cssText="background-image: url(images/sahil-moosa-q5O9V6yZFuk-unsplash.jpg);"){
-        credit.innerHTML ='Photo by <a href="https://unsplash.com/ja/@sahilkv_?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText%22%3ESahil Moosa</a> on <a href="https://unsplash.com/photos/q5O9V6yZFuk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText%22%3EUnsplash</a>';
-    }else if(imageContainer.style.cssText="background-image: url(images/tharoushan-kandarajah-KtDXt7DyfVM-unsplash.jpg);"){
-        credit.innerHTML='Photo by <a href="https://unsplash.com/@tharoushan?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText%22%3ETharoushan Kandarajah</a> on <a href="https://unsplash.com/photos/KtDXt7DyfVM?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText%22%3EUnsplash</a>';
-    }
+const imageContainer = document.querySelector('.image-container');
+const creditTag = document.querySelector('.credit');
+
+let index = 0;
+
+function updateImage() {
+    const {src, credit} = images[index];
+    const duration = 16; // duration of the animation in seconds
+    imageContainer.style.transition = `background-image ${duration}s ease-in-out`;
+    creditTag.style.transition = `opacity ${duration}s ease-in-out`;
+
+    imageContainer.style.backgroundImage = `url(${src})`;
+    creditTag.innerHTML = `Photo by ${credit}`;
+
+    index = (index + 1) % images.length;
 }
+
+updateImage();
+setInterval(updateImage, 4000);
